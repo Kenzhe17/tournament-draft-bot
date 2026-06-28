477f54105caf7fa6977d6908172d72c7c2a89505
Kenzhe Toishyman <230103298@sdu.edu.kz>
2026-06-28 16:24:42 +0500

    Move /elo command to tournament group as /tournament elo

--- cogs/tournament.py
+++ cogs/tournament.py
@@ -410,7 +410,7 @@ class TournamentCog(commands.Cog):
 
         await interaction.edit_original_response(embed=embed)
 
-    @app_commands.command(name="elo", description="Изменить ELO игрока")
+    @tournament_group.command(name="elo", description="Изменить ELO игрока")
     @app_commands.default_permissions(administrator=True)
     @app_commands.describe(
         player="Игрок",
