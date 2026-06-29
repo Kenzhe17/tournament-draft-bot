ce1b7275c8a7f78f08bd0817ec4ff7b3c458df30
Kenzhe Toishyman <230103298@sdu.edu.kz>
2026-06-29 15:52:01 +0500

    Update matchmaking __init__.py with proper exports

--- matchmaking/__init__.py
+++ matchmaking/__init__.py
@@ -1,1 +1,6 @@
 """Matchmaking system."""
+
+from matchmaking.manager import matchmaking_manager
+from matchmaking.session import MatchmakingSession
+
+__all__ = ["matchmaking_manager", "MatchmakingSession"]
